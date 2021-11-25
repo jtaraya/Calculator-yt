4cae31a09637a9792e251632c107ad889bc66180
jtaraya <jacobtaraya@gmail.com>
2021-11-25 10:29:51 +0300

    added constact to query selector

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const numberButtons 
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButtons = document.querySelector('[data-equals]')
+const deleteButtons = document.querySelector('[data-delete]')
+const allClearButtons = document.querySelector('[data-allClear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
